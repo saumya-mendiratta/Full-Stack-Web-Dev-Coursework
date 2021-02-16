7064890f0073c58a2d9a3869eaac4abceffee28b
saumya-mendiratta <saya.mendiratta@gmail.com>
2021-02-17 00:24:45 +0530

    Create let.js

---
+++ JAVASCRIPT/ES6 FEATURES/let.js
@@ -0,0 +1,43 @@
+// -------------Let---------------
+
+//var a=10;
+
+// if(a<=10){
+//     var b=40;
+// }else{
+//     var c=50;
+// }
+// console.log(a,b,c);
+
+
+// if(a<=10){
+//     let b=40;
+// }else{
+//     let c=50;
+// }
+// console.log(a,b,c);
+
+// if(a<=10){
+//     console.log(b);
+//          let b=40;
+// }
+
+// let b;
+// console.log(b);
+// b=10;
+
+
+// -------------Let VS var---------------
+
+// for(var i=1; i<=5;i++){
+//     setTimeout(function(){
+//       console.log(i);
+//     } , 1000);
+// }
+
+for(let i=1; i<=5;i++){
+    setTimeout(function(){
+      console.log(i);
+    } , 1000);
+}
+
